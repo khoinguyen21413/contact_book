3d5bd675aedeafa6a9a5b0bf8bf418c93d9721b6
khoinguyen21413 <khoinguyen.21413@gmail.com>
2025-02-28 19:19:52 +0700

    add frame title

--- index.py
+++ index.py
@@ -4,14 +4,25 @@ import tkinter as tk
 
 # Tạo cửa sổ chính
 root = tk.Tk()
-root.title("Ứng dụng quản lý danh bạ")
+root.title("Ứng dụng quản lý danh bạ-by khoinguyen-v1")
 root.geometry("800x420")
 root.configure(background="#242424")
 
+# Tạo các biến
+colour_white = "white"
+colour_black = "#242424"
+
 # Tạo các widget
-# label tiêu đề chính
-label_main_content = tk.Label(root, bg="#242424", font=("Arial", 14, "bold"), text= "Quản lý danh bạ", fg="white")
-label_main_content.pack(fill="x")
+# Tạo frame chứa tiêu đề
+frame_title = tk.Frame(root, bg=colour_black)
+frame_title.pack(fill="x", pady= 5)
+
+# Label tiêu đề chính
+title_label = tk.Label(frame_title, bg=colour_black, font=("Arial", 14, "bold"), text= "Quản lý danh bạ", fg="white")
+title_label.pack(pady=5)
+
+# Tạo frame chứa bảng danh bạ
+
 
 # Bố trí widget trên giao diện
 
