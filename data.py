c0a78b202ccfbd731a1a34ca1de8519004dc7222
khoinguyen21413 <khoinguyen.21413@gmail.com>
2025-03-14 19:37:34 +0700

    add samplate data

--- data.py
+++ data.py
@@ -6,4 +6,9 @@ sample_contacts = [
     Contact("Trần", "Bình", "Nam", "0987 654 321", "tranbinh@gmail.com", "TP Hồ Chí Minh"),
     Contact("Phạm", "Thảo", "Nữ", "0971 223 456", "phamthao99@gmail.com", "Đà Nẵng"),
     Contact("Hoàng", "Dũng", "Nam", "0904 778 899", "hoangdung@gmail.com", "Hải Phòng"),
+    Contact("Vũ", "Linh", "Nữ", "0965 112 223", "vulinh@gmail.com", "Cần Thơ"),
+    Contact("Đặng", "Hải", "Nam", "0933 567 890", "danghai@gmail.com", "Hà Nội"),
+    Contact("Bùi", "Trang", "Nữ", "0978 345 123", "buitrang@gmail.com", "Hồ Chí Minh"),
+    Contact("Ngô", "Khang", "Nam", "0902 222 444", "ngokhang@gmail.com", "Đà Nẵng"),
+    Contact("Phan", "Như", "Nữ", "0981 567 999", "phannhu@gmail.com", "Hải Phòng"),
 ]
